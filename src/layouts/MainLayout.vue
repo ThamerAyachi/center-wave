eed5441cc1ab611855467ab00aeead2067b1e9a3
Thamer Ayachi <thamerjak3@gmail.com>
2022-08-27 00:48:45 +0100

    add base mainLayout page

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex h-screen bg-gray-200 font-roboto">
+    <Sidebar />
+
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <Header />
+
+      <main
+        class="flex-1 overflow-x-hidden overflow-y-auto bg-darked text-gray-300"
+      >
+        <div class="container mx-auto px-6 py-8">
+          <router-view />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from "../components/Dashboard/Sidebar.vue";
+import Header from "../components/Dashboard/Header.vue";
+
+export default {
+  components: {
+    Sidebar,
+    Header,
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
+
+import MainLayout from "../layouts/MainLayout.vue";
+
 import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
 
 const routes = [
+  {
+    path: "/content",
+    name: "MainLayout",
+    component: MainLayout,
+    children: [{ path: "/about", name: "About", component: AboutView }],
+  },
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
