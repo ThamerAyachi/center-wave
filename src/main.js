a851c881192d9083ebc0bfe373cf5a3844ec8a95
Thamer Ayachi <thamerjak3@gmail.com>
2022-08-27 16:07:22 +0100

    add animate js

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import router from "./router";
 import store from "./store";
 import "./assets/style.css";
 import "./assets/tailwind.css";
+import "animate.css";
 
 createApp(App).use(store).use(router).mount("#app");
