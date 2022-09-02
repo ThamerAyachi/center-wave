050aa6bddb85aecd87acee0fbdc9147fe890883b
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-02 20:33:15 +0100

    change name page from createPost to newPage

--- src/components/Dashboard/Sidebar.vue
+++ src/components/Dashboard/Sidebar.vue
@@ -95,7 +95,7 @@ export default {
       ],
       browseMusic: [
         { icon: icons.musicalNote, name: "Posts", text: "Posts" },
-        { icon: icons.musicalNote, name: "CreatePost", text: "Create Post" },
+        { icon: icons.musicalNote, name: "CreatePost", text: "New Post" },
       ],
     };
   },
