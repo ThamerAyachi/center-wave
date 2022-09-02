9b8887bab0d1c59850d4f3c58170f409f79db70b
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-02 20:39:15 +0100

    create box for content in new post page

--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <div class="w-full text-center text-3xl font-semibold">New Post</div>
+    <div class="flex justify-center">
+      <div class="bg-dark my-10" style="max-width: 700px"></div>
+    </div>
   </div>
 </template>
 
