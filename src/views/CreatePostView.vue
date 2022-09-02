10c355c0e430000a87d51cc9342d879808f964ca
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-02 20:53:48 +0100

    add search input

--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -2,7 +2,18 @@
   <div>
     <div class="w-full text-center text-3xl font-semibold">New Post</div>
     <div class="flex justify-center">
-      <div class="bg-dark my-10" style="max-width: 700px"></div>
+      <div class="my-10" style="max-width: 700px">
+        <!-- search -->
+        <form class="px-3 my-5 flex w-full">
+          <input
+            type="search"
+            class="p-2 bg-dark w-4/5 text-primary rounded-l-xl focus:outline-none border-dark focus:border-primary focus:ring focus:ring-opacity-20 focus:ring-primary"
+          />
+          <button class="w-1/5 bg-dark text-primary rounded-r-xl">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
