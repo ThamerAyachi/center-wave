9236dcf7a25157c850bb55cbf434dbc73dd3ffea
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-04 22:21:18 +0100

    add search method in store

--- src/store/search.js
+++ src/store/search.js
@@ -1,9 +1,21 @@
-// import axios from "axios";
+import axios from "axios";
 
 const search = {
   state: () => ({}),
 
-  actions: {},
+  actions: {
+    searchMusic: async (t, data) => {
+      try {
+        const res = await axios.get(
+          "https://shazam.p.rapidapi.com/search",
+          data
+        );
+        return res;
+      } catch (err) {
+        return err;
+      }
+    },
+  },
 };
 
 export default search;
