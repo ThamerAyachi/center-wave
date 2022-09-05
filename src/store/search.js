dd362c70c103630814cfae59741f9bf8713beb8f
Thamer Ayachi <thamerjak3@gmail.com>
2022-09-05 22:39:37 +0100

    add new api from spotify

--- src/store/search.js
+++ src/store/search.js
@@ -6,9 +6,23 @@ const search = {
   actions: {
     searchMusic: async (t, data) => {
       try {
-        const res = await axios.get("https://shazam.p.rapidapi.com/search", {
-          term: data,
-        });
+        const options = {
+          method: "GET",
+          url: "https://spotify23.p.rapidapi.com/search/",
+          params: {
+            q: data,
+            type: "multi",
+            offset: "0",
+            limit: "10",
+            numberOfTopResults: "5",
+          },
+          headers: {
+            "X-RapidAPI-Key":
+              "c876ef2714mshe5d52a32c229f64p1c923ajsnb585710c31f3",
+            "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
+          },
+        };
+        const res = await axios.request(options);
         return res;
       } catch (err) {
         return err;
